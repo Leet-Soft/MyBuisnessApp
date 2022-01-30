1cb58d8f0b40ed467316fc4800e25b53d24aaab3
Jordan <jtodorov@leet-soft.com>
2022-01-30 09:56:41 +0200

    .

--- app/src/main/java/uni/fmi/masters/mybuisnessapp/LoginActivity.java
+++ app/src/main/java/uni/fmi/masters/mybuisnessapp/LoginActivity.java
@@ -2,11 +2,16 @@ package uni.fmi.masters.mybuisnessapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import uni.fmi.masters.mybuisnessapp.db.BuissnessDBHelper;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,6 +20,9 @@ public class LoginActivity extends AppCompatActivity {
     TextView registerTV;
     CheckBox rememberMeCB;
     Button loginB;
+
+    BuissnessDBHelper db;
+    int count = 3;
 
 
     @Override
@@ -28,5 +36,37 @@ public class LoginActivity extends AppCompatActivity {
         rememberMeCB = findViewById(R.id.rememberCheckBox);
         loginB = findViewById(R.id.loginButton);
 
+        loginB.setOnClickListener(onClickListener);
+
+        db = new BuissnessDBHelper(this);
+
+        registerTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            String email = usernameET.getText().toString();
+            String password = passwordET.getText().toString();
+
+            if(db.checkCredentials(email, password)){
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }else{
+                Toast.makeText(LoginActivity.this, "Invalid Credentials", Toast.LENGTH_LONG).show();
+                count--;
+
+                if(count == 0){
+                    finish();
+                }
+            }
+        }
+    };
 }
